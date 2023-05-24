0f76e0bf696b0bf418c6a557e0c97fea50875d8f
Elias Moura <elias.smourayt@gmail.com>
2023-05-24 13:26:20 -0300

    adicionando autenticação

--- jogoteca.py
+++ jogoteca.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, flash, redirect, render_template, request, session
+
 
 class Jogo:
     def __init__(self, nome, categoria, console) -> None:
         self.nome = nome
         self.categoria = categoria
         self.console = console
-    
+
     def __str__(self):
         return f'{self.nome}'
 
@@ -16,10 +17,12 @@ jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
 jogos = [jogo1, jogo2, jogo3]
 
 app = Flask(__name__)
+app.secret_key = 'alura'
+
 
 @app.route('/')
 def index():
-    return render_template('lista.html',titulo='Jogos', jogos=jogos)
+    return render_template('lista.html', titulo='Jogos', jogos=jogos)
 
 
 @app.route('/criar', methods=['POST',])
@@ -35,5 +38,29 @@ def criar():
 @app.route('/novo')
 def novo():
     return render_template('novo.html', titulo='Novo jogo')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if 'batatinha' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(f'{session["usuario_logado"]} logado com sucesso.')
+        return redirect('/')
+    else:
+        flash('Usuário não logado')
+        return redirect('/login')
+
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso.')
+    return redirect('/')
+
 
 app.run(debug=True)
