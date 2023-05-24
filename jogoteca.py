c368a7379f96e5113fe89f6c0d1945d68b3103f7
Elias-Moura <elias.smourayt@gmail.com>
2023-05-24 08:02:15 -0300

    Aplicando bootstrap e extendendo templates

--- jogoteca.py
+++ jogoteca.py
@@ -1,10 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+
+class Jogo:
+    def __init__(self, nome, categoria, console) -> None:
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+    
+    def __str__(self):
+        return f'{self.nome}'
+
+
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('GTA V', 'Mundo aberto', 'Xbox 360/PS3')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+jogos = [jogo1, jogo2, jogo3]
 
 app = Flask(__name__)
 
-@app.route('/inicio')
-def olar():
-    return render_template('lista.html')
-   
+@app.route('/')
+def index():
+    return render_template('lista.html',titulo='Jogos', jogos=jogos)
 
-app.run()
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo_gerado = Jogo(nome, categoria, console)
+    jogos.append(jogo_gerado)
+    return redirect('/')
+
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo jogo')
+
+app.run(debug=True)
